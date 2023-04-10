e25ad1708f6862854f026e9abb5f6736b1afdf85
Andrey <teryaev.2004@mail.ru>
2023-04-10 23:52:03 +0300

    configure-express

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import express, { Express } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+import mongoose from "mongoose";
+
+const app: Express = express();
+
+const startServer = async () => {
+  try {
+    await mongoose.connect(process.env.mongoUrl!);
+
+    app.listen(5000, () => {
+      console.log("SERVER STARTED");
+    });
+  } catch (err) {
+    console.log("Server Error", err);
+    process.exit(1);
+  }
+};
+
+startServer();
