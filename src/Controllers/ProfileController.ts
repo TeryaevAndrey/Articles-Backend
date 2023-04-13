26a8025458923ae88f01496423be211f7e51c80d
Andrey <teryaev.2004@mail.ru>
2023-04-13 12:56:00 +0300

    add-edit-profile-route-and-finish-editProfile-method

--- src/Controllers/ProfileController.ts
+++ src/Controllers/ProfileController.ts
@@ -1,23 +1,64 @@
 import UserModel from "../models/UserModel.js";
-import {Request, Response} from "express";
+import { Request, Response } from "express";
+import { cloudinary } from "../utils/cloudinary.js";
+import bcrypt from "bcrypt";
 
 class ProfileController {
-    editProfile = async(req: Request, res: Response) => {
-        try {
-            const {email, userName, password, passwordRepeat}: {
-                email?: string;
-                userName?: string;
-                password?: string;
-                passwordRepeat?: string;
-            } = req.body;
+  editProfile = async (req: Request, res: Response) => {
+    try {
+      const {
+        email,
+        userName,
+        password,
+        oldPassword,
+      }: {
+        email?: string;
+        userName?: string;
+        password?: string;
+        oldPassword?: string;
+      } = req.body;
 
-            const avatar = req.file;
+      const avatar = req.file;
 
-            
-        } catch(err) {
-            return res.status(500).json({message: "Ошибка сервера"});
+      const user = await UserModel.findOne({ _id: req.userId });
+
+      let avatarFile = null;
+      let matchedOldsPasswords: Promise<boolean> | boolean = false;
+      let newHashedPassword: string = user.password;
+
+      if (password) {
+        newHashedPassword = await bcrypt.hash(password, 12);
+      }
+
+      if (oldPassword) {
+        matchedOldsPasswords = await bcrypt.compare(oldPassword, user.password);
+      } else {
+        return res
+          .status(404)
+          .json({ message: "Введите старый пароль, чтобы его обновить" });
+      }
+
+      if (avatar) {
+        avatarFile = await cloudinary.v2.uploader.upload(avatar.path, {
+          folder: "articles-avatars",
+        });
+      }
+
+      await UserModel.updateOne(
+        { _id: req.userId },
+        {
+          avatar: avatar || user.avatar,
+          email: email || user.email,
+          userName: userName || user.userName,
+          password: matchedOldsPasswords ? newHashedPassword : user.password,
         }
+      );
+
+      return res.json({ message: "Данные обновлены" });
+    } catch (err) {
+      return res.status(500).json({ message: "Ошибка сервера" });
     }
+  };
 }
 
 export default ProfileController;
--- src/core/routes.ts
+++ src/core/routes.ts
@@ -2,9 +2,11 @@ import { Express, urlencoded, json } from "express";
 import AuthController from "../Controllers/AuthController.js";
 import checkAuth from "../middlewares/checkAuth.js";
 import ArticlesController from "../Controllers/ArticlesController.js";
+import ProfileController from "../Controllers/ProfileController.js";
 
 const createRoutes = (app: Express) => {
   const AuthCtrl = new AuthController();
+  const ProfileCtrl = new ProfileController();
   const ArticleCtrl = new ArticlesController();
 
   app.use(urlencoded({ extended: false }));
@@ -12,6 +14,8 @@ const createRoutes = (app: Express) => {
 
   app.post("/auth/reg", AuthCtrl.reg);
   app.post("/auth/login", AuthCtrl.login);
+
+  app.post("/edit-profile", checkAuth, ProfileCtrl.editProfile);
 
   app.post("/add-article", checkAuth, ArticleCtrl.addArticle);
 };
