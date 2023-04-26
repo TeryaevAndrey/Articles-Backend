3ebec303745fdf76395a3d0846c67af50f28a0c5
Andrey <teryaev.2004@mail.ru>
2023-04-26 22:08:07 +0300

    fix-CommentSchema-(add-timestamps)

--- src/models/CommentModel.ts
+++ src/models/CommentModel.ts
@@ -1,12 +1,15 @@
 import mongoose, { Schema, model } from "mongoose";
 import { IComment } from "../types.js";
 
-const CommentSchema = new Schema<IComment>({
-  from: { type: Schema.Types.ObjectId, require: true, ref: "User" },
-  articleId: { type: Schema.Types.ObjectId, require: true },
-  rating: { type: Number, require: true },
-  text: { type: String },
-});
+const CommentSchema = new Schema<IComment>(
+  {
+    from: { type: Schema.Types.ObjectId, require: true, ref: "User" },
+    articleId: { type: Schema.Types.ObjectId, require: true },
+    rating: { type: Number, require: true },
+    text: { type: String },
+  },
+  { timestamps: true }
+);
 
 const UserModel = model("Comment", CommentSchema);
 
